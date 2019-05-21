b235523908f89b2da7a1bb0931aadcfc9979577f
3355844 <3355844@gmail.com>
2019-05-21 12:40:14 +0300

    html_J218

--- src/main/java/com/test/demo/StartController.java
+++ src/main/java/com/test/demo/StartController.java
@@ -1,4 +1,14 @@
 package com.test.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class StartController {
+
+    @GetMapping("/")
+    public String list() {
+        return "index";
+    }
+
 }
