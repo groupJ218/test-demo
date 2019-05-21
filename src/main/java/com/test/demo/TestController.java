340e99749476844cb343f186c9da4be69fb822d8
3355844 <3355844@gmail.com>
2019-05-21 12:21:12 +0300

    initial commit

---
+++ src/main/java/com/test/demo/TestController.java
@@ -0,0 +1,17 @@
+package com.test.demo;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class TestController {
+
+    @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Widget index() {
+        return new Widget("green", 10, 7);
+    }
+
+}
---
+++ src/main/java/com/test/demo/Widget.java
@@ -0,0 +1,29 @@
+package com.test.demo;
+
+public class Widget {
+
+    private String type;
+
+    private int length;
+
+    private int height;
+
+    public Widget(String type, int length, int height) {
+        this.type = type;
+        this.length = length;
+        this.height = height;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
+
