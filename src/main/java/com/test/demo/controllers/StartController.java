7ddb3e0b58795cfd6d7bccffef3100b06d365edd
3355844 <3355844@gmail.com>
2019-06-13 21:29:28 +0300

    Change start controller

--- src/main/java/com/test/demo/controllers/StartController.java
+++ src/main/java/com/test/demo/controllers/StartController.java
@@ -1,12 +1,14 @@
 package com.test.demo.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RequestMapping("/")
+@RestController
 public class StartController {
 
-    @GetMapping("/")
+    @RequestMapping(method = RequestMethod.GET)
     public String list() {
         return "index";
     }
