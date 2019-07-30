d514e2488f450378da81194278092ca2ff53ffb4
3355844 <3355844@gmail.com>
2019-07-30 21:41:36 +0300

    Agg libs for Logging and JSON converter

--- src/main/java/com/test/demo/controllers/UserController.java
+++ src/main/java/com/test/demo/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.test.demo.controllers;
 
-import com.google.gson.Gson;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.test.demo.models.User;
 import com.test.demo.services.UserService;
 import org.springframework.stereotype.Controller;
@@ -11,28 +12,32 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
-import java.io.PrintWriter;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    private Gson gson = new Gson();
-
-    @Resource(name="UserService")
+    Logger log = Logger.getLogger(UserController.class.getName());
+    @Resource(name = "UserService")
     private UserService userService;
 
-    // Displaying the initial users list.
+    // Displaying the initial users list.(/user/list)
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String getPersons(Model model) {
-        System.out.println("list url");
+        log.info("Start get User List method");
         List user_list = userService.getAll();
-        model.addAttribute("users", user_list);
-        System.out.println(user_list.toString());
-        String userListString = this.gson.toJson(user_list);
-        System.out.println(userListString);
-        return userListString;
+        ObjectMapper mapper = new ObjectMapper();
+        String userListJson = null;
+        try {
+            userListJson = mapper.writeValueAsString(user_list);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        model.addAttribute("users", userListJson);
+        log.warning("Users: "+ user_list.toString());
+        return "user";
     }
 
     // Opening the add new user form page.
@@ -45,7 +50,7 @@ public class UserController {
 
     // Opening the edit user form page.
     @RequestMapping(value = "/edit", method = RequestMethod.GET)
-    public String editUser(@RequestParam(value="id", required=true) String id, Model model) {
+    public String editUser(@RequestParam(value = "id", required = true) String id, Model model) {
         System.out.println("edit url");
         model.addAttribute("userAttr", userService.findUserId(id));
         return "index";
@@ -53,7 +58,7 @@ public class UserController {
 
     // Deleting the specified user.
     @RequestMapping(value = "/delete", method = RequestMethod.GET)
-    public String delete(@RequestParam(value="id", required=true) String id, Model model) {
+    public String delete(@RequestParam(value = "id", required = true) String id, Model model) {
         System.out.println("delete URL");
         userService.delete(id);
         return "redirect:list";
@@ -62,7 +67,7 @@ public class UserController {
     // Adding a new user or updating an existing user.
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String save(@ModelAttribute("userAttr") User user) {
-        if(user.getId() != null && !user.getId().trim().equals("")) {
+        if (user.getId() != null && !user.getId().trim().equals("")) {
             userService.edit(user);
         } else {
             userService.add(user);
